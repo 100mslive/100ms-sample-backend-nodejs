2058791bda34b5020be58645c8f3ad8148bea353
Karthikeyan S <karthikeyanssvk@gmail.com>
2023-04-02 13:01:26 +0530

    add list sessions example

--- README.md
+++ README.md
@@ -16,59 +16,63 @@ This is a sample backend app built on [Node.js](https://nodejs.org/en/) with [Ex
 - Get usage analytics
 
 ## Get started
+
 Deploy directly to Render using this button. But don't forget to add the environment variables specified in `.env.example` to Render or the build will fail!
 
 [![Deploy to Render](https://render.com/images/deploy-to-render-button.svg)](https://render.com/deploy?repo=https://github.com/100mslive/100ms-sample-backend-nodejs)
 
 ## Installation
+
 1. Clone the repo
 
-    ```
-    git clone https://github.com/100mslive/100ms-sample-backend-nodejs.git
-    ```
+   ```
+   git clone https://github.com/100mslive/100ms-sample-backend-nodejs.git
+   ```
 
 2. Install the dependencies
 
-    ```
-    npm i
-    ```
+   ```
+   npm i
+   ```
 
 3. Rename the `.env.example` to `.env` and add your credentials
 
-    ```
-    APP_ACCESS_KEY=<YOUR_APP_ACCESS_KEY>
-    APP_SECRET=<YOUR_APP_SECRET>
-    ```
+   ```
+   APP_ACCESS_KEY=<YOUR_APP_ACCESS_KEY>
+   APP_SECRET=<YOUR_APP_SECRET>
+   ```
 
 4. Run the app
 
-    Running in dev mode (hot reload on file changes):
+   Running in dev mode (hot reload on file changes):
 
-    ```
-    npm run start:dev
-    ```
+   ```
+   npm run start:dev
+   ```
 
-    Running in production:
+   Running in production:
 
-    ```
-    npm run start
-    ```
+   ```
+   npm run start
+   ```
 
-    Run with Docker Compose
+   Run with Docker Compose
 
-    ```
-    docker compose up --build
-    ```
+   ```
+   docker compose up --build
+   ```
 
 ## Usage
 
 This sample exposes the following endpoints
 
-| Endpoint | Method | Parameters | Description |
-|---|---|---|---|
-| `/create-room` | POST | JSON body <pre>{<br>&nbsp;&nbsp;"name": "new-room-1",<br>&nbsp;&nbsp;"description": "Sample description",<br>&nbsp;&nbsp;"template_id": "6318xxxxxxxxxxxxxxxc60"<br>}</pre>| Create a new room with room params ([docs](https://www.100ms.live/docs/server-side/v2/Rooms/create-via-api)) |
-| `/auth-token` | POST | JSON body <br><pre>{<br>&nbsp;&nbsp;"room_id": "632ecxxxxxxxxxxxxxxxx764",<br>&nbsp;&nbsp;"role": "host",<br>&nbsp;&nbsp;"user_id":"test_user"<br>}</pre>| Generate an auth token for a peer to join a room ([docs](https://www.100ms.live/docs/server-side/v2/introduction/authentication-and-tokens)) |
-| `/session-analytics-by-room` | GET | Query params <pre>?room_id=633dxxxxxxxxxxxxxxx7d1d2</pre>| Usage analytics for a specific session ([docs](https://www.100ms.live/docs/server-side/v2/Sessions/example-build-attendance)) |
+| Endpoint                     | Method | Parameters                                                                                                                                                                  | Description                                                                                                                               |
+| ---------------------------- | ------ | --------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | ----------------------------------------------------------------------------------------------------------------------------------------- |
+| `/create-room`               | POST   | JSON body <pre>{<br>&nbsp;&nbsp;"name": "new-room-1",<br>&nbsp;&nbsp;"description": "Sample description",<br>&nbsp;&nbsp;"template_id": "6318xxxxxxxxxxxxxxxc60"<br>}</pre> | Create a new room with room params ([docs](https://www.100ms.live/docs/server-side/v2/api-reference/Rooms/create-via-api))                |
+| `/auth-token`                | POST   | JSON body <br><pre>{<br>&nbsp;&nbsp;"room_id": "632ecxxxxxxxxxxxxxxxx764",<br>&nbsp;&nbsp;"role": "host",<br>&nbsp;&nbsp;"user_id":"test_user"<br>}</pre>                   | Generate an auth token for a peer to join a room ([docs](https://www.100ms.live/docs/concepts/v2/concepts/security-and-tokens))           |
+| `/session-analytics-by-room` | GET    | Query params <pre>?room_id=633dxxxxxxxxxxxxxxx7d1d2</pre>                                                                                                                   | Usage analytics for a specific session ([docs](https://www.100ms.live/docs/server-side/v2/how--to-guides/build-attendance))               |
+| `/session-list-by-room`      | GET    | Query params <pre>?room_id=633dxxxxxxxxxxxxxxx7d1d2</pre>                                                                                                                   | Get list of all sessions associated with a room ([docs](https://www.100ms.live/docs/server-side/v2/api-reference/Sessions/list-sessions)) |
 
 ## Docs
+
 Refer to the 100ms [server-side docs](https://www.100ms.live/docs/server-side/v2/introduction/basics) to get started on your custom backend.
--- src/app.js
+++ src/app.js
@@ -1,105 +1,142 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const { APIService } = require('./services/APIService');
-const { TokenService } = require('./services/TokenService');
+const { APIService } = require("./services/APIService");
+const { TokenService } = require("./services/TokenService");
 
-let express = require('express');
+let express = require("express");
 let app = express();
 app.use(express.json()); // for parsing request body as JSON
 let port = process.env.PORT || 3000;
 
-const moment = require('moment');
+const moment = require("moment");
 
 const tokenService = new TokenService();
 const apiService = new APIService(tokenService);
 
 // Create a new room, either randomly or with the requested configuration
-app.post('/create-room', async (req, res) => {
-    const payload = {
-        "name": req.body.name,
-        "description": req.body.description,
-        "template_id": req.body.template_id,
-        "region": req.body.region
-    };
-    try {
-        const roomData = await apiService.post("/rooms", payload);
-        res.json(roomData);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Internal Server Error");
-    }
-
+app.post("/create-room", async (req, res) => {
+  const payload = {
+    name: req.body.name,
+    description: req.body.description,
+    template_id: req.body.template_id,
+    region: req.body.region,
+  };
+  try {
+    const roomData = await apiService.post("/rooms", payload);
+    res.json(roomData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 // Generate an auth token for a peer to join a room
-app.post('/auth-token', (req, res) => {
-    try {
-        const token = tokenService.getAuthToken({ room_id: req.body.room_id, user_id: req.body.user_id, role: req.body.role });
-        res.json({
-            token: token,
-            msg: "Token generated successfully!",
-            success: true,
-        });
-    } catch (error) {
-        res.json({
-            msg: "Some error occured!",
-            success: false,
-        });
-    }
+app.post("/auth-token", (req, res) => {
+  console.log(tokenService.getManagementToken());
+  try {
+    const token = tokenService.getAuthToken({
+      room_id: req.body.room_id,
+      user_id: req.body.user_id,
+      role: req.body.role,
+    });
+    res.json({
+      token: token,
+      msg: "Token generated successfully!",
+      success: true,
+    });
+  } catch (error) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 // Get usage analytics for the latest Session in a Room
-app.get('/session-analytics-by-room', async (req, res) => {
-    try {
-        const sessionListData = await apiService.get("/sessions", { room_id: req.query.room_id });
-        if (sessionListData.data.length > 0) {
-            const sessionData = sessionListData.data[0];
-            console.log(sessionData);
+app.get("/session-analytics-by-room", async (req, res) => {
+  try {
+    const sessionListData = await apiService.get("/sessions", {
+      room_id: req.query.room_id,
+    });
+    if (sessionListData.data.length > 0) {
+      const sessionData = sessionListData.data[0];
+      console.log(sessionData);
 
-            // Calculate individual participants' duration
-            const peers = Object.values(sessionData.peers);
-            const detailsByUser = peers.reduce((acc, peer) => {
-                const duration = moment
-                    .duration(moment(peer.left_at).diff(moment(peer.joined_at)))
-                    .asMinutes();
-                const roundedDuration = Math.round(duration * 100) / 100;
-                acc[peer.user_id] = {
-                    "name": peer.name,
-                    "user_id": peer.user_id,
-                    "duration": (acc[peer.user_id] || 0) + roundedDuration
-                };
-                return acc;
-            }, {});
-            const result = Object.values(detailsByUser);
-            console.log(result);
+      // Calculate individual participants' duration
+      const peers = Object.values(sessionData.peers);
+      const detailsByUser = peers.reduce((acc, peer) => {
+        const duration = moment
+          .duration(moment(peer.left_at).diff(moment(peer.joined_at)))
+          .asMinutes();
+        const roundedDuration = Math.round(duration * 100) / 100;
+        acc[peer.user_id] = {
+          name: peer.name,
+          user_id: peer.user_id,
+          duration: (acc[peer.user_id] || 0) + roundedDuration,
+        };
+        return acc;
+      }, {});
+      const result = Object.values(detailsByUser);
+      console.log(result);
 
-            // Calculate aggregated participants' duration
-            const totalDuration = result
-                .reduce((a, b) => a + b.duration, 0)
-                .toFixed(2);
-            console.log(`Total duration for all peers: ${totalDuration} minutes`);
+      // Calculate aggregated participants' duration
+      const totalDuration = result
+        .reduce((a, b) => a + b.duration, 0)
+        .toFixed(2);
+      console.log(`Total duration for all peers: ${totalDuration} minutes`);
 
-            // Calculate total session duration
-            const sessionDuration = moment
-                .duration(moment(sessionData.updated_at).diff(moment(sessionData.created_at)))
-                .asMinutes()
-                .toFixed(2);
-            console.log(`Session duration is: ${sessionDuration} minutes`);
+      // Calculate total session duration
+      const sessionDuration = moment
+        .duration(
+          moment(sessionData.updated_at).diff(moment(sessionData.created_at))
+        )
+        .asMinutes()
+        .toFixed(2);
+      console.log(`Session duration is: ${sessionDuration} minutes`);
 
-            res.json({
-                "user_duration_list": result,
-                "session_duration": sessionDuration,
-                "total_peer_duration": totalDuration
-            });
-        } else {
-            res.status(404).send("No session found for this room");
-        }
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Internal Server Error");
+      res.json({
+        user_duration_list: result,
+        session_duration: sessionDuration,
+        total_peer_duration: totalDuration,
+      });
+    } else {
+      res.status(404).send("No session found for this room");
     }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Get the list of all sessions in a room
+app.get("/session-list-by-room", async (req, res) => {
+  try {
+    let allSessions = [];
+    let last;
+    while (true) {
+      const filters = { room_id: req.query.room_id, limit: 20 };
+      // Check if we have a `last` value from the previous iteration
+      if (last) {
+        // If yes, set it as the `start` value for the next iteration
+        filters.start = last;
+      }
+      // Get the list of sessions
+      const someSessionListData = await apiService.get("/sessions", filters);
+      // If there are no more sessions: break
+      if (!someSessionListData.data || someSessionListData.data.length == 0) {
+        break;
+      }
+      allSessions = allSessions.push(someSessionListData.data);
+      // If there are less than `limit` sessions, no need to iterate again: break
+      if (someSessionListData.data.length < 20) break;
+      // Set the `last` value for the next iteration
+      last = someSessionListData.last;
+    }
+    res.json(allSessions);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Token server started on ${port}!`);
+  console.log(`Token server started on ${port}!`);
 });
